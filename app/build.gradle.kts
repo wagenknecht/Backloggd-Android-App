4252a7dd0bea148bc61772cfad7f86e2c6568005
wagenknecht <wgnkncht@gmail.com>
2024-07-05 16:13:02 +0200

    webview added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/de/wagenknecht/backloggd/MainActivity.java
@@ -0,0 +1,23 @@
+package de.wagenknecht.backloggd;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://backloggd.com/");
+    }
+}
