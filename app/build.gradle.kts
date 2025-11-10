d968cc28d149f4d3aa27b1c227a7f4a0c0f4f748
wagenknecht <70338036+wagenknecht@users.noreply.github.com>
2025-11-10 16:49:02 +0100

    increase version to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.4.1"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
