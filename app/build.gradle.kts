6a88441d006462850f1a26cdaa5490be65f7fa7f
wagenknecht <70338036+wagenknecht@users.noreply.github.com>
2026-05-26 13:28:19 +0200

    New version 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.wagenknecht.backloggd"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.5"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
